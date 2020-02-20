d71afc061fdc6e144cc0f9a924eee476321a89be
github username <github email address>
2020-02-20 15:49:52 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((response) => {
+        return response.json();
+    })
+    .then((myJson) => {
+        console.log(myJson);
+    });
+  });
